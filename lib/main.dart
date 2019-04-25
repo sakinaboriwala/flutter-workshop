b66819489ca502c4e5813a2b35b600f08d86fa22
SakinaBoriwala21 <sakina.boriwala21@gmail.com>
2019-04-25 18:48:12 +0530

    Make HTTP API call to firebase backend
    - get the list of movie titles and images
    - render the ListView builder with data received from backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -21,36 +25,66 @@ class MovieCards extends StatefulWidget {
 }
 
 class _MovieCardsState extends State<MovieCards> {
+  List<Map> movies = [];
+
+  getData() {
+    http
+        .get('https://flutter-workshop-april.firebaseio.com/movies.json')
+        .then((response) {
+      json.decode(response.body).forEach((dynamic movieId, dynamic movieData) {
+        setState(() {
+          movies.add(movieData);
+        });
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Movies'),
-        ),
-        body: ListView.builder(
-          itemCount: 3,
-          itemBuilder: (BuildContext context, int index) {
-            return movieCard();
-          },
-        ));
+      appBar: AppBar(
+        title: Text('Movies'),
+      ),
+      body: ListView.builder(
+        itemCount: movies.length,
+        itemBuilder: (BuildContext context, int index) {
+          return movieCard(index);
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.white,
+        mini: true,
+        child: Icon(Icons.movie),
+        onPressed: () {
+          getData();
+        },
+      ),
+    );
   }
 
-  Widget movieCard() {
+  Widget movieCard(int index) {
     return Card(
-      margin: EdgeInsets.all(10),
+      margin: EdgeInsets.all(20),
       elevation: 2.0,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      child: Column(
+      child: Stack(
         children: <Widget>[
-          Text(
-            'Avengers EndGame',
-            style: TextStyle(
-                fontSize: 30, fontWeight: FontWeight.bold, color: Colors.blue),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Image.asset('images/avengers.jpg'),
+          Image.network(movies[index]['image']),
+          Positioned(
+            bottom: 0.0,
+            width: double.maxFinite,
+            child: Container(
+              color: Colors.black45,
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: Text(
+                movies[index]['title'].toString().toUpperCase(),
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
