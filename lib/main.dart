6e28ab7c1af3b7f2f380182e9fd31afc12628251
SakinaBoriwala21 <sakina.boriwala21@gmail.com>
2019-04-27 09:48:56 +0530

    stateless-widget

--- lib/main.dart
+++ lib/main.dart
@@ -8,44 +8,33 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData.dark(),
       title: 'Movie Cards',
-      home: MovieCards(),
-    );
-  }
-}
-
-class MovieCards extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _MovieCardsState();
-  }
-}
-
-class _MovieCardsState extends State<MovieCards> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Movies'),
-        ),
-        body: Column(children: [movieCard()]));
-  }
-
-  Widget movieCard() {
-    return Card(
-      margin: EdgeInsets.all(10),
-      elevation: 2.0,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      child: Column(
-        children: <Widget>[
-          Text(
-            'Avengers EndGame',
-            style: TextStyle(
-                fontSize: 30, fontWeight: FontWeight.bold, color: Colors.blue),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Movies'),
           ),
-          SizedBox(height: 10,),
-          Image.asset('images/avengers.jpg'),
-        ],
-      ),
+          body: Column(children: [
+            Card(
+              margin: EdgeInsets.all(10),
+              elevation: 2.0,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12)),
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    'Avengers EndGame',
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Image.asset('images/avengers.jpg'),
+                ],
+              ),
+            ),
+          ])),
     );
   }
 }
